cc0995c12cb4d61500ded4db6b017a92074c7331
Mohamed Kalanthar Hussain <jaheerkalanthar816@gmail.com>
2021-05-10 14:56:25 +0530

    Date Picker and workk on date

--- src/components/treeComponents/Search.js
+++ src/components/treeComponents/Search.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import "./Search.css"
-
+import React,{useState} from 'react';
+import "./Search.css";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 //Date Picker Components
 function Search() {
+    //To use the datePicker we need two important variable
+    //1.Start Date
+    const [startDate,setstartDate]=useState(new Date())
+    //1.End Date
+    const [endDate,setendDate]=useState(new Date())
     return (
         <div className="Search">
             <h1>Hello world</h1>
