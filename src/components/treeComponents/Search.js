34492182e3614e1cbacbc995569d4221af9f1b13
Mohamed Kalanthar Hussain <jaheerkalanthar816@gmail.com>
2021-05-10 16:36:43 +0530

    Stylish date Picker

--- src/components/treeComponents/Search.js
+++ src/components/treeComponents/Search.js
@@ -1,18 +1,40 @@
-import React,{useState} from 'react';
+import React,{ useState } from 'react';
 import "./Search.css";
 import { DateRangePicker } from "react-date-range";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
+import { Button } from "@material-ui/core"
+import { PeopleIcon } from "@material-ui/icons/People"
+
+
 //Date Picker Components
+
 function Search() {
     //To use the datePicker we need two important variable
     //1.Start Date
-    const [startDate,setstartDate]=useState(new Date())
+    const [startDate,setStartDate]=useState(new Date())
     //1.End Date
-    const [endDate,setendDate]=useState(new Date())
+    const [endDate,setEndDate]=useState(new Date())
+
+    //Selection Range
+    const selectionRange={
+        startDate:startDate,
+        endDate: endDate,
+        key:"selection"
+    };
+
+    //The below function will handle the selection
+
+
+    function handleSelect(ranges){
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+    }
+
     return (
         <div className="Search">
-            <h1>Hello world</h1>
+            <DateRangePicker ranges={[selectionRange]} onChange={handleSelect}></DateRangePicker>
+            <h2>Number Of Guests <PeopleIcon></PeopleIcon> </h2>
         </div>
     )
 }
